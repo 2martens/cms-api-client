51183b8c777b996d0ff56d5bbb645d6e04d1502c
Jim Martens <jim1@live.de>
2014-01-26 20:41:57 +0100

    [Git] Fixed indentation and git calls
    * Converted tabs to spaces
    * Exchanged call with check_output

--- src/git/branches.py
+++ src/git/branches.py
@@ -1,19 +1,19 @@
-from subprocess import call
+from subprocess import check_output
 
 def create(name, base):
     'Creates a branch branching off of base'
-    call('git checkout ' + base, shell=True)
-    call('git checkout -b ' + name, shell=True)
+    check_output('git checkout ' + base, shell=True)
+    check_output('git checkout -b ' + name, shell=True)
 
 def checkout(name):
     'Checkout branch name'
-    call('git checkout ' + name, shell=True)
+    check_output('git checkout ' + name, shell=True)
 
 def remove(name):
     'Removes a branch from the repository'
-    call('git branch -d ' + name, shell=True)
+    check_output('git branch -d ' + name, shell=True)
 
 def merge(base, head):
     'Merges branch head into base'
     branches.checkout_branch(base)
-    call('git merge --no-ff ' + head, shell=True)
+    check_output('git merge --no-ff ' + head, shell=True)
--- src/git/staging.py
+++ src/git/staging.py
@@ -1,17 +1,17 @@
-from subprocess import call
+from subprocess import check_output
 
 def add(filename='.'):
-	'Adds the given file to the staging index; by default recursively the whole directory'
-	call('git add ' + filename, shell=True)
+    'Adds the given file to the staging index; by default recursively the whole directory'
+    check_output('git add ' + filename, shell=True)
 
 def remove(filename):
-	'Marks the given file as removed in the staging index'
-	call('git rm ' + filename, shell=True)
+    'Marks the given file as removed in the staging index'
+    check_output('git rm ' + filename, shell=True)
 
-def reset():
-	'Resets the complete staging index'
-	call('git reset', shell=True)
+def reset_all():
+    'Resets the complete staging index'
+    check_output('git reset', shell=True)
 
-def reset(filename):
-	'Removes the given file from the staging index'
-	call('git reset ' + filename, shell=True)
+def reset_file(filename):
+    'Removes the given file from the staging index'
+    check_output('git reset ' + filename, shell=True)
