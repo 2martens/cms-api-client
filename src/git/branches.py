11975661d736d3ae3cab297a097e4002631d26ec
Jim Martens <jim1@live.de>
2014-01-26 18:44:25 +0100

    [Git] Improved naming
    * Renamed function names in branches and tags

--- src/git/branches.py
+++ src/git/branches.py
@@ -1,19 +1,19 @@
 from subprocess import call
 
-def create_branch(name, base):
+def create(name, base):
     'Creates a branch branching off of base'
     call('git checkout ' + base, shell=True)
     call('git checkout -b ' + name, shell=True)
 
-def checkout_branch(name):
+def checkout(name):
     'Checkout branch name'
     call('git checkout ' + name, shell=True)
 
-def remove_branch(name):
+def remove(name):
     'Removes a branch from the repository'
     call('git branch -d ' + name, shell=True)
 
-def merge_branch(base, head):
+def merge(base, head):
     'Merges branch head into base'
     branches.checkout_branch(base)
     call('git merge --no-ff ' + head, shell=True)
--- src/git/tags.py
+++ src/git/tags.py
@@ -1,12 +1,12 @@
 from subprocess import call
 
-def create_tag(name, comment=''):
+def create(name, comment=''):
 	'Creates a tag with the given name and optionally comment'
 	if (comment):
 		call('git tag -m ' + comment + ' ' + name, shell=True)
 	else:
 		call('git tag ' + name, shell=True)
 
-def delete_tag(name):
+def remove(name):
 	'Deletes a tag with the given name'
 	call('git tag -d ' + name)
