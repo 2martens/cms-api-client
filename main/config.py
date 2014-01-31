75c18fb826b72b25cf6e78452b27dcefff50e416
Jim Martens <jim1@live.de>
2014-01-31 14:09:42 +0100

    [Main] Added config dialog class
    * Improved Config class

--- main/config.py
+++ main/config.py
@@ -1,20 +1,24 @@
 import configparser
+from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit
+from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QGridLayout
 
 class Config:
     'Config accessor class'
     def __init__(self, configfile='config.ini'):
         self.__configfile = configfile
+        self.__config = self.__read()
 
     def get(self, section, key):
         'Returns a config value'
-        config = self.__read())
-        return config[section][key]
+        return self.__config[section][key]
 
     def set(self, section, key, value):
         'Sets a config value'
-        config = self.__read()
-        config[section][key] = value
-        self.__write(config)
+        self.__config[section][key] = value
+
+    def write(self):
+        'Writes the settings'
+        self.__write()
 
     def __read(self):
         'Returns the config parser for the config file'
@@ -23,8 +27,48 @@ class Config:
             config.read_file(configfile)
         return config
 
-    def __write(self, config):
+    def __write(self):
         'Writes the config file for the given config'
         with open(self.__configfile, 'w') as configfile:
-            config.write(configfile)
+            self.__config.write(configfile)
 
+class ConfigDialog(QDialog):
+    'Dialog class for configuration dialog'
+    def __init__(self, parent=None):
+        'Initializes the dialog'
+        super(ConfigDialog, self).__init__(parent)
+
+        self.__config = Config()
+
+        apiLabel = QLabel('API-Key')
+        self.apiLine = QLineEdit()
+        self.apiline.setText(self.__config.get('General', 'apikey'))
+        gitDirLabel = QLabel('Git directory')
+        self.gitDirLine = QLineEdit()
+        self.gitDirLine.setText(self.__config.get('Git', 'dir'))
+        self.submitButton = QPushButton('&Submit')
+
+        buttonLayout = QVBoxLayout()
+        buttonLayout.addWidget(apiLabel)
+        buttonLayout.addWidget(self.apiLine)
+        buttonLayout.addWidget(gitDirLabel)
+        buttonLayout.addWidget(self.gitDirLine)
+        buttonLayout.addWidget(self.submitButton)
+
+        self.submitButton.clicked.connect(self.submitForm)
+
+        mainLayout = QGridLayout()
+        # mainLayout.addWidget(nameLabel, 0, 0)
+        mainLayout.addLayout(buttonLayout, 0, 1)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle('Configuration')
+
+    def submitForm(self):
+        'Submits the form'
+        apiKey = self.apiLine.text()
+        gitDir = self.gitDirLine.text()
+
+        self.__config.set('Git', 'dir', gitDir)
+        self.__config.set('General', 'apikey', apiKey)
+        self.__config.write()
