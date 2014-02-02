3ab9bfda59bb4e97a942284925266213cfa99d78
Jim Martens <jim1@live.de>
2014-02-02 18:38:50 +0100

    [IO] Removed type and engine arguments

--- src/io/files.py
+++ src/io/files.py
@@ -1,11 +1,11 @@
 
-def read_file(name, type='html', engine='twig'):
+def read_file(name):
 	'Returns the content of the specified file'
-	file = open(name + '.' + type + '.' + engine, 'r', encoding='utf-8')
+	file = open(name, 'r', encoding='utf-8')
 	return file.read(None)
 
-def write_file(name, content, type='html', engine='twig'):
+def write_file(name, content):
 	'Writes the content into the specified file (overwrites if file exists)'
-	file = open(name + '.' + type + '.' + engine, 'w', encoding='utf-8')
+	file = open(name, 'w', encoding='utf-8')
 	file.write(content)
 
