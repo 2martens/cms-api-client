1d70d34941e7d8a1fd3478c573a1dbe69da53f30
Jim Martens <jim1@live.de>
2014-02-02 18:18:07 +0100

    [Main] Added repo module
    * Added TreeView class that inherits QTreeView

---
+++ main/repo.py
@@ -0,0 +1,38 @@
+from PyQt5.QtWidgets import QTreeView, QFileSystemModel
+from main.config import Config
+
+class TreeView(QTreeView):
+    'Represents the categories, contents, etc in the git repository'
+    def __init__(self, parent=0):
+        'Initializes the tree view'
+        super(TreeView, self).__init__(parent)
+        self.__config = Config()
+        self.__gitDir = self.__config.get('Git', 'dir')
+        self.__model = QFileSystemModel()
+        self.__model.setRootPath(self.__gitDir)
+        self.setModel(self.__model)
+        self.setRootIndex(self.__model.index(self.__gitDir))
+        self.__selectionModel = self.selectionModel()
+        self.__selectionModel.currentChanged.connect(self.__update)
+
+        # initialize listener
+        self.__listener = {}
+
+    def getSelectedFile(self):
+        'Returns the currently selected file'
+        return self.__currentFile
+
+    def addListener(self, key, function):
+        'Adds a listener function to the list of listener'
+        self.__listener[key] = function
+
+    def removeListener(self, key):
+        'Removes a listener from the list of listener'
+        self.__listener[key] = None
+
+    def __update(self, currentIndex, previousIndex):
+        'Called when the current item in the tree view changes'
+        if (not self.__model.isDir(currentIndex)):
+            self.__currentFile = self.__model.fileName(currentIndex)
+            for listener in self.__listener:
+                listener(self.__currentFile)
